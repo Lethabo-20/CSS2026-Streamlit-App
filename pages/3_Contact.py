2ab0f93a36730cb939b45b34b172327910ba2d58
LethaboP <142802533+Lethabo-20@users.noreply.github.com>
2026-02-06 14:31:31 +0200

    Create 3_Contact.py

---
+++ pages/3_Contact.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Contact Information")
+
+st.write("Let's connect! I am open to collaborations and professional opportunities.")
+
+st.markdown("""
+- **LinkedIn:** [Lethabo Chantel Phalime](http://www.linkedin.com/in/lethabochantelphalime-a64661245)
